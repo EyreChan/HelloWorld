935d7d94c25ca7c83ab71ae209b6bdbfb57b399a
Eyre Chan <1294023567@qq.com>
2019-03-04 23:47:44 +0800

    django admin

--- HelloWorld/urls.py
+++ HelloWorld/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url
+from django.contrib import admin
 from . import view, search
 
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^hello$', view.hello),
     url(r'^search-form$', search.search_form),
     url(r'^search$', search.search),
